c9845a4dccb4cea518fddf4bc7fad497649ce8ac
imbharatchawla <bharat.chawla@scoopwhoop.com>
2019-11-21 16:39:38 +0530

    added variables file

---
+++ Code Snippets/variables.py
@@ -0,0 +1,39 @@
+x = 5 #type of int
+y = "John" #type of string
+print(x)
+print(y)
+
+x = "John"
+#Both are same
+x = 'John'
+
+x, y, z = 'Orange', 'Banana', 'Cherry'
+print(x)
+print(y)
+print(z)
+
+
+x = "awesome"
+
+print('Python is ' + x)
+
+x = 'Python is'
+y = 'awesome'
+z = x + y
+print(z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+# print(x + y) #this will give error, int and string cant be concatenated
+
+x = 5
+y = "John"
+print(str(x) + y) #this is right way
+
+
+
+
