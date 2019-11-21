943c0b33af94f787d60253f1825dce1a0f251217
imbharatchawla <bharat.chawla@scoopwhoop.com>
2019-11-21 16:03:55 +0530

    added comment file

---
+++ Code Snippets/comment.py
@@ -0,0 +1,22 @@
+#This is a comment
+print('Hello World!')
+
+#This is a 
+#Multi line comment
+print('Print afet Multi line comments')
+
+"""Hello this is also
+considered as a
+comment in 
+python
+language
+using ..
+as long as this string is not assigned to a variable, python will 
+read it but ignore it
+"""
+print('Hey!')
+
+print('Good Morning!') #Comments can be written this way as well
+
+#print('Hello World!) this is a commented code
+
